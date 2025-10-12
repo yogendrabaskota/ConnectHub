5a15d76976fbb6941842f0daad08798bdcaf3e58
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-10-12 08:53:43 +0545

    refactor with ts

---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import app from "./src/app";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+  });
+});
+
+export default app;
